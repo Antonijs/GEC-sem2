ee81aaccc59fabd9a64819d9b6a57d04c811a012
Antonijs Jemeljanovs <91891431+Antonijs@users.noreply.github.com>
2022-03-08 12:41:47 +0000

    Need to add block to GameScreen1

--- GEC-sem2/GEC-sem2/Texture2D.cpp
+++ GEC-sem2/GEC-sem2/Texture2D.cpp
@@ -59,3 +59,6 @@ void Texture2D::Render(Vector2D newPosition, SDL_RendererFlip flip, double angle
 	//Render Screen
 	SDL_RenderCopyEx(m_renderer, m_texture, NULL, &renderLocation, 0, NULL, flip);
 }
+void Texture2D::Render(SDL_Rect src_rect, SDL_Rect src_dest, SDL_RendererFlip flip, double angle) {
+    SDL_RenderCopyEx(m_renderer, m_texture, &src_rect, &src_dest, angle, nullptr, flip);
+}
